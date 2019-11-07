ca6032805bd6687b297d256c7e27fe9feede536c
Dmytro Bondarchuk <dmbo@opinov8.com>
2019-11-07 18:29:09 +0200

    Test for RedisServerProvider

---
+++ TattleTrail/TattleTrail/Tests/DALTests/RedisServerProviderTests/Builder.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using StackExchange.Redis;
+using TattleTrail.DAL.RedisServerProvider;
+
+namespace TattleTrail.Tests.DALTests.RedisServerProviderTests {
+    public class Builder {
+        private IConnectionMultiplexer _multiplexer = Mock.Of<IConnectionMultiplexer>();
+
+        public Builder WithConnectionMultiplexer(IConnectionMultiplexer multiplexer) {
+            _multiplexer = multiplexer;
+            return this;
+        }
+
+        public RedisServerProvider Build() {
+            return new RedisServerProvider(_multiplexer);
+        }
+    }
+}
---
+++ TattleTrail/TattleTrail/Tests/DALTests/RedisServerProviderTests/when_constructor_called.cs
@@ -0,0 +1,41 @@
+﻿using Machine.Specifications;
+using Moq;
+using StackExchange.Redis;
+using System;
+using System.Net;
+using TattleTrail.DAL.RedisServerProvider;
+using It = Machine.Specifications.It;
+
+namespace TattleTrail.Tests.DALTests.RedisServerProviderTests {
+    //TODO: Rework test
+    [Subject(typeof(RedisServerProvider))]
+    [Ignore("Rework Later")]
+    public class when_constructor_called {
+        Establish _context = () => {
+            multiplexer = Mock.Of<IConnectionMultiplexer>(x => x.GetEndPoints(false) == new EndPoint[] { });
+            provider = new Builder()
+            .WithConnectionMultiplexer(multiplexer)
+            .Build();
+
+        };
+
+        It should_call_get_server_once = () => {
+            Mock.Get(multiplexer).Verify(x => x.GetServer(Moq.It.IsAny<String>(), null), Times.Once);
+        };
+
+        It should_call_get_database_once = () => {
+            Mock.Get(multiplexer).Verify(x => x.GetDatabase(-1, null), Times.Once);
+        };
+
+        It should_set_server_as_property = () => {
+            provider.Server.Equals(multiplexer.GetServer(Moq.It.IsAny<String>(), null));
+        };
+
+        It should_set_database_as_property = () => {
+            provider.Database.Equals(multiplexer.GetDatabase(-1, null));
+        };
+
+        static IRedisServerProvider provider;
+        static IConnectionMultiplexer multiplexer;
+    }
+}
