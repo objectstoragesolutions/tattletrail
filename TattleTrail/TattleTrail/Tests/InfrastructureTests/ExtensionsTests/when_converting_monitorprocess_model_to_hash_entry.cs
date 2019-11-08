c84f9bc52a734532f4012a29aec4269875a4f1e1
Dmytro Bondarchuk <dmbo@opinov8.com>
2019-11-08 13:54:51 +0200

    Extensions tests
    Extensions were covered with tests

---
+++ TattleTrail/TattleTrail/Tests/InfrastructureTests/ExtensionsTests/when_converting_monitorprocess_model_to_hash_entry.cs
@@ -0,0 +1,50 @@
+﻿using AutoFixture;
+using Machine.Specifications;
+using Moq;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using TattleTrail.Infrastructure.Extensions;
+using TattleTrail.Models;
+using It = Machine.Specifications.It;
+
+namespace TattleTrail.Tests.InfrastructureTests.ExtensionsTests {
+    [Subject(typeof(MonitorProcessModelExtensions))]
+    public class when_converting_monitorprocess_model_to_hash_entry {
+
+        Establish _context = () => {
+            Fixture fixture = new Fixture();
+            var lifeTime = fixture.Create<int>();
+            var processName = fixture.Create<String>();
+            monitor = Mock.Of<MonitorProcess>(
+                x => x.Id == Guid.NewGuid() && 
+                x.LifeTime == lifeTime && 
+                x.ProcessName == processName && 
+                x.Subscribers == new HashSet<string>());
+        };
+
+        Because of = () => 
+            result = monitor.ConvertMonitorToHashEntry();
+
+        It should_contain_process_field_name = () =>
+            result[0].Name.Equals(nameof(monitor.ProcessName));
+
+        It should_contain_process_field_value = () =>
+            result[0].Value.Equals(monitor.ProcessName);
+
+        It should_contain_lifeTime_field_name = () => 
+            result[1].Name.Equals(nameof(monitor.LifeTime));
+
+        It should_contain_lifeTime_field_value = () =>
+            result[1].Value.Equals(monitor.LifeTime);
+
+        It should_contain_subscribers_field_name =() =>
+            result[2].Name.Equals(nameof(monitor.Subscribers));
+
+        It should_contain_subscribers_field_value = () =>
+             result[2].Value.Equals(monitor.Subscribers);
+
+        static MonitorProcess monitor;
+        static HashEntry[] result;
+    }
+}
---
+++ TattleTrail/TattleTrail/Tests/InfrastructureTests/ExtensionsTests/when_converting_user_model_to_hash_entry.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture;
+using Machine.Specifications;
+using Moq;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using TattleTrail.Infrastructure.Extensions;
+using TattleTrail.Models;
+using It = Machine.Specifications.It;
+
+namespace TattleTrail.Tests.InfrastructureTests.ExtensionsTests {
+    [Subject(typeof(UserModelExtensions))]
+    public class when_converting_user_model_to_hash_entry {
+        Establish _context = () => {
+            Fixture fixture = new Fixture();
+            var userName = fixture.Create<String>();
+            user = Mock.Of<User>(
+                x => x.Id == Guid.NewGuid() &&
+                x.UserName == userName &&
+                x.MonitorProcessId == new HashSet<Guid>());
+        };
+
+        Because of = () =>
+            result = user.ConvertUserToHashEntry();
+
+        It should_contain_userid_field_name = () =>
+            result[0].Name.Equals(nameof(user.Id));
+
+        It should_contain_userid_field_value = () =>
+            result[0].Value.Equals(user.Id.ToByteArray());
+
+        It should_contain_username_field_name = () =>
+            result[1].Name.Equals(nameof(user.UserName));
+
+        It should_contain_username_field_value = () =>
+            result[1].Value.Equals(user.UserName);
+
+        It should_contain_processid_field_name = () =>
+             result[2].Name.Equals(nameof(user.MonitorProcessId));
+
+        It should_contain_processid_field_value = () =>
+             result[2].Value.Equals(user.MonitorProcessId);
+
+        static User user;
+        static HashEntry[] result;
+    }
+}
