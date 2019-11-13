7f1de0958ef7c6be8d2fcdb2e9fb1e645d111ea0
Dmytro Bondarchuk <dmbo@opinov8.com>
2019-11-13 16:51:47 +0200

    Email service added
    Interface and implementation (is not working yet).

---
+++ TattleTrail/TattleTrail/Infrastructure/EmailService/EmailService.cs
@@ -0,0 +1,27 @@
+﻿using MimeKit;
+using System;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+
+namespace TattleTrail.Infrastructure.EmailService {
+    public class EmailService : IEmailService {
+        public async Task SendEmailAsync(String subscribers, string subject, string message) {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Warning!", "dmbo@opinov8.ru"));
+            emailMessage.To.Add(new MailboxAddress($"{subscribers}"));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) {
+                Text = message
+            };
+
+            using (var client = new SmtpClient()) {
+                await client.ConnectAsync("", 25, false);
+                await client.AuthenticateAsync("", "");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
---
+++ TattleTrail/TattleTrail/Infrastructure/EmailService/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿using System.Threading.Tasks;
+
+namespace TattleTrail.Infrastructure.EmailService {
+    public interface IEmailService {
+        Task SendEmailAsync(string subscribers, string subject, string message);
+    }
+}
