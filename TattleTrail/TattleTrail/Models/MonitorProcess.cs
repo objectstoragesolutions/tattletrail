494f705959aa57900855e463222e6f5f682dd495
Dmytro Bondarchuk <dmbo@opinov8.com>
2019-11-18 17:34:01 +0200

    Dockerfile and docker-compose
    v1 is ready

--- TattleTrail/TattleTrail/Models/MonitorProcess.cs
+++ TattleTrail/TattleTrail/Models/MonitorProcess.cs
@@ -3,7 +3,6 @@
 namespace TattleTrail.Models {
     public class MonitorProcess {
         public Guid Id { get; set; }
-
         public MonitorDetails MonitorDetails { get; set; } = new MonitorDetails();
     }
 }
